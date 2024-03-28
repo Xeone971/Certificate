d6913fbbadaa0c45717651334ffce0e88248ff6b
Titan Epicure <collas.lucca@outlook.fr>
2024-03-28 13:16:14 +0100

    Ajout données des pathways

---
+++ js/tab.js
@@ -0,0 +1,129 @@
+let pathTrad = {
+    path1: ['Research and Development Managers in Food Packaging basic'],
+    path2: ['Research and Development Managers inFood Packaging advanced'],
+    path3: ['Quality Managers inFood Packaging basic'],
+    path4: ['Quality Managers in Food Packaging advanced'],
+    path5: ['Packaging Production Line Workers basic'],
+    path6: ['Packaging Production Managers basic'],
+    path7: ['Packaging Production Managers advanced']
+};
+
+let path1 = {
+    FPA: ['FP-A-1'],
+    FPB: ['FP-B-1', 'FP-B-2', 'FP-B-3', 'FP-B-4', 'FP-B-5', 'FP-B-6', 'FP-B-7'],
+    FPC: ['FP-C-1 ', 'FP-C-2', 'FP-C-3', 'FP-C-4', 'FP-C-5', 'FP-C-10'],
+    FPD: ['FP-D-1', 'FP-D-2', 'FP-D-3', 'FP-D-4', 'FP-D-5', 'FP-D-6', 'FP-D-7', 'FP-D-8', 'FP-D-9'],
+    FPE: ['FP-E-1', 'FP-E-2', 'FP-E-3', 'FP-E-4', 'FP-E-5', 'FP-E-6', 'FP-E-7', 'FP-E-8', 'FP-E-9', 'FP-E-10', 'FP-E-11'],
+    FPF: ['FP-F-1', 'FP-F-2', 'FP-F-3', 'FP-F-4', 'FP-F-5', 'FP-F-6'],
+    FPH: ['FP-H-1', 'FP-H-2', 'FP-H-5', 'FP-H-8'],
+    FPI: ['FP-I-3']
+};
+let path1Trad = {
+    FPA: ['Panorama of Food packaging'],
+    FPB: ['Food Packaging Materials'],
+    FPC: ['Food Packaging Properties'],
+    FPD: ['Food Packaging Line Processes'],
+    FPE: ['Food Packaging and Shelf-life'],
+    FPF: ['Food Packaging Innovations'],
+    FPH: ['Food Packaging Regulation and Safety Issues'],
+    FPI: ['Modelling Tools']
+};
+
+let path2 = {
+    FPC: ['FP-C-6', 'FP-C-7', 'FP-C-8', 'FP-C-9'],
+    FPG: ['FP-G-1', 'FP-G-2', 'FP-G-3', 'FP-G-4', 'FP-G-5', 'FP-G-6', 'FP-G-7', 'FP-G-8', 'FP-G-9', 'FP-G-10', 'FP-G-11'],
+    FPH: ['FP-H-3', 'FP-H-5', 'FP-H-6', 'FP-H-7', 'FP-H-8', 'FP-H-9'],
+    FPI: ['FP-I-1', 'FP-I-2', 'FP-I-3', 'FP-I-4', 'FP-I-5', 'FP-I-6', 'FP-I-7', 'FP-I-8', 'FP-I-9', 'FP-I-10', 'FP-I-11']
+};
+
+let path2Trad = {
+    FPC: ['Food Packaging Properties'],
+    FPG: ['Food Packaging and Environment'],
+    FPH: ['Food Packaging Regulation and Safety Issues'],
+    FPI: ['Modelling Tools']
+};
+let path3 = {
+    FPA: ['FP-A-1'],
+    FPB: ['FP-B-1', 'FP-B-2', 'FP-B-3', 'FP-B-4', 'FP-B-5', 'FP-B-6', 'FP-B-7'],
+    FPC: ['FP-C-1 ', 'FP-C-2', 'FP-C-3', 'FP-C-4', 'FP-C-5'],
+    FPD: ['FP-D-1', 'FP-D-2', 'FP-D-3', 'FP-D-4', 'FP-D-5', 'FP-D-6', 'FP-D-7', 'FP-D-8', 'FP-D-9'],
+    FPE: ['FP-E-1', 'FP-E-2', 'FP-E-3', 'FP-E-4', 'FP-E-5', 'FP-E-6', 'FP-E-7', 'FP-E-8', 'FP-E-9', 'FP-E-10', 'FP-E-11'],
+    FPF: ['FP-F-1', 'FP-F-2', 'FP-F-3', 'FP-F-4', 'FP-F-5', 'FP-F-6'],
+    FPH: ['FP-H-1', 'FP-H-2', 'FP-H-5', 'FP-H-7'],
+    FPI: ['FP-I-3'],
+};
+
+let path3Trad = {
+    FPA: ['Panorama of Food Packaging'],
+    FPB: ['Food Packaging Materials'],
+    FPC: ['Food Packaging Properties'],
+    FPD: ['Food Packaging Line Processes'],
+    FPE: ['Food Packaging and Shelf-life'],
+    FPF: ['Food Packaging Innovations'],
+    FPH: ['Food Packaging Regulation and Safety Issues'],
+    FPI: ['Modelling Tools'],
+};
+
+let path4 = {
+    FPC: ['FP-C-6', 'FP-C-7', 'FP-C-8', 'FP-C-9'],
+    FPE: ['FP-E-1', 'FP-E-2', 'FP-E-3', 'FP-E-4', 'FP-E-5', 'FP-E-6', 'FP-E-7', 'FP-E-8', 'FP-E-9', 'FP-E-10', 'FP-E-11'],
+    FPG: ['FP-G-5'],
+    FPI: ['FP-I-1', 'FP-I-2']
+};
+
+let path4Trad = {
+    FPC: ['Food Packaging Properties'],
+    FPE: ['Food Packaging and Shelf-life'],
+    FPG: ['Food Packaging and Environment'],
+    FPI: ['Modelling Tools']
+};
+
+let path5 = {
+    FPA: ['FP-A-1'],
+    FPB: ['FP-B-1', 'FP-B-2', 'FP-B-3', 'FP-B-4', 'FP-B-5', 'FP-B-6', 'FP-B-7'],
+    FPC: ['FP-C-1 ', 'FP-C-2', 'FP-C-3', 'FP-C-4', 'FP-C-5'],
+    FPD: ['FP-D-1', 'FP-D-2', 'FP-D-3', 'FP-D-4', 'FP-D-5', 'FP-D-6', 'FP-D-7', 'FP-D-8', 'FP-D-9'],
+    FPE: ['FP-E-2'],
+    FPH: ['FP-H-4', 'FP-H-7', 'FP-H-10']
+};
+
+let path5Trad = {
+    FPA: ['Panorama of Food Packaging'],
+    FPB: ['Food Packaging Materials'],
+    FPC: ['Food Packaging Properties'],
+    FPD: ['Food Packaging Line Processes'],
+    FPE: ['Food Packaging and Shelf-life'],
+    FPH: ['Food Packaging Regulation and Safety Issues']
+};
+
+let path6 = {
+    FPA: ['FP-A-1'],
+    FPB: ['FP-B-1', 'FP-B-2', 'FP-B-3', 'FP-B-4', 'FP-B-5', 'FP-B-6', 'FP-B-7'],
+    FPC: ['FP-C-1 ', 'FP-C-2', 'FP-C-3', 'FP-C-4', 'FP-C-5', 'FP-C-10'],
+    FPD: ['FP-D-1', 'FP-D-2', 'FP-D-3', 'FP-D-4', 'FP-D-5', 'FP-D-6', 'FP-D-7', 'FP-D-8', 'FP-D-9', 'FP-D-10'],
+    FPE: ['FP-E-1', 'FP-E-2', 'FP-E-3', 'FP-E-4', 'FP-E-5', 'FP-E-6', 'FP-E-7', 'FP-E-8', 'FP-E-9', 'FP-E-10', 'FP-E-11'],
+    FPF: ['FP-F-1', 'FP-F-2', 'FP-F-3', 'FP-F-4', 'FP-F-5', 'FP-F-6'],
+    FPH: ['FP-H-1', 'FP-H-9']
+};
+
+let path6Trad = {
+    FPA: ['Panorama of Food Packaging'],
+    FPB: ['Food Packaging Materials '],
+    FPC: ['Food Packaging Properties'],
+    FPD: ['Food Packaging Line Processes'],
+    FPE: ['Food Packaging and Shelf-life'],
+    FPF: ['Food Packaging Innovations'],
+    FPH: ['Food Packaging Regulation and Safety Issues']
+};
+
+let path7 = {
+    FPD: ['FP-D-11', 'FP-D-12'],
+    FPG: ['FP-G-2', 'FP-G-12'],
+    FPH: ['FP-H-5', 'FP-H-11', 'FP-I-8']
+};
+
+let path7Trad = {
+    FPD: ['Food Packaging Line Processes'],
+    FPG: ['Food Packaging and Environment'],
+    FPH: ['Food Packaging Regulation and Safety Issues']
+};
